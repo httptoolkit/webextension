07ac9a63a5a7b3cc18d1d34833f03aa9c0dcc7d8
Tim Perry <pimterry@gmail.com>
2022-08-01 19:07:08 +0200

    Improve build: clean output, show stats, avoid maps in prod
    I'd actually quite like to include sourcemaps in prod, but they're huge.
    It's cheaper to just not minify (making the bundle messy but readable)
    and drop maps entirely. We keep them for dev though.
    
    This takes the raw extension size to just under 1MB, or 130kb gzipped,
    which is a comfortable bump to add to the total server size (and
    probably shrinks further in a full server bundle anyway).

--- esbuild.js
+++ esbuild.js
@@ -1,18 +1,20 @@
 const esbuild = require("esbuild");
+const { clean } = require('esbuild-plugin-clean');
 
 const { NodeModulesPolyfillPlugin } = require('@esbuild-plugins/node-modules-polyfill');
 const { NodeGlobalsPolyfillPlugin } = require('@esbuild-plugins/node-globals-polyfill');
 
-esbuild
-    .build({
+(async () => {
+    const result = await esbuild.build({
         entryPoints: [
             "./src/background.ts",
             "./src/content-script.ts",
             "./src/injected-script.ts"
         ],
         bundle: true,
-        sourcemap: true,
+        metafile: true,
         outdir: "./public/build",
+        sourcemap: !!process.env.DEV_MODE,
         watch: !!process.env.DEV_MODE,
         external: ['brotli-wasm'],
         plugins: [
@@ -20,7 +22,11 @@ esbuild
             NodeGlobalsPolyfillPlugin({
                 process: true,
                 buffer: true
-            })
+            }),
+            clean({ patterns: ['./public/build/**/*'] })
         ]
     })
     .catch(() => process.exit(1));
+
+    console.log(await esbuild.analyzeMetafile(result.metafile));
+})();
